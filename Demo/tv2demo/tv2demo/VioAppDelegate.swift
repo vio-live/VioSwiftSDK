baefe9f1dd8ff00909230ac164e8966c7ca033d0
Angelo Sepulveda <angelosepulveda@angelos-air.lan>
2026-03-05 23:25:31 +0100

    fix: registerIfNeeded en Task MainActor + userId angelo_demo_001 para match cart-intent

--- Demo/tv2demo/tv2demo/VioAppDelegate.swift
+++ Demo/tv2demo/tv2demo/VioAppDelegate.swift
@@ -8,7 +8,9 @@ class VioAppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         UNUserNotificationCenter.current().delegate = self
-        DeviceTokenManager.shared.registerIfNeeded()
+        Task { @MainActor in
+            DeviceTokenManager.shared.registerIfNeeded()
+        }
         return true
     }
 
